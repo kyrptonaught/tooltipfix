2dc4752a8ba4cf2ca20992d8bd023e869119663c
kyrptonaught <anthonybuttillo10@gmail.com>
2020-10-06 02:47:11 -0400

    Cleanup

---
+++ src/main/java/net/kyrptonaught/tooltipfix/Helper.java
@@ -0,0 +1,45 @@
+package net.kyrptonaught.tooltipfix;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Helper {
+    public static List<Text> doFix(List<Text> text, int x, int width, TextRenderer textRenderer) {
+        text = new ArrayList<>(text);
+        for (int i = 0; i < text.size(); i++) {
+            if (isTooWide(x, width, textRenderer, text.get(i).getString())) {
+                Style style = text.get(i).getStyle();
+                List<String> words = new ArrayList<>(Arrays.asList(text.get(i).getString().split(" ")));
+
+                String newLine = words.remove(0);
+                if (isTooWide(x, width, textRenderer, newLine)) {
+                    String oldLine = newLine;
+                    while (isTooWide(x, width, textRenderer, newLine + "-"))
+                        newLine = newLine.substring(0, newLine.length() - 1);
+                    words.add(0, "-" + oldLine.substring(newLine.length()));
+                    newLine = newLine + "-";
+
+                } else
+                    while (words.size() > 0)
+                        if (!isTooWide(x, width, textRenderer, newLine + " " + words.get(0)))
+                            newLine += " " + words.remove(0);
+                        else break;
+
+                text.set(i, new LiteralText(newLine).setStyle(style));
+                if (words.size() > 0)
+                    text.add(i + 1, new LiteralText(String.join(" ", words)).setStyle(style));
+            }
+        }
+        return text;
+    }
+
+    private static boolean isTooWide(int x, int width, TextRenderer textRenderer, String line) {
+        return x + textRenderer.getWidth(line) > width - 20;
+    }
+}
--- src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
+++ src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
@@ -1,14 +1,12 @@
 package net.kyrptonaught.tooltipfix.mixin;
 
 import com.google.common.collect.Lists;
+import net.kyrptonaught.tooltipfix.Helper;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.OrderedText;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -29,48 +27,13 @@ public abstract class FixToolTipMixin {
 
     @Inject(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;II)V", at = @At(value = "HEAD"), cancellable = true)
     public void fix(MatrixStack matrices, Text text, int x, int y, CallbackInfo ci) {
-        this.renderOrderedTooltip(matrices, Lists.transform(doFix(Collections.singletonList(text), x), Text::asOrderedText), x, y);
+        this.renderOrderedTooltip(matrices, Lists.transform(Helper.doFix(Collections.singletonList(text), x, width, textRenderer), Text::asOrderedText), x, y);
         ci.cancel();
     }
 
     @Inject(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;II)V", at = @At("HEAD"), cancellable = true)
     public void fix(MatrixStack matrices, List<Text> lines, int x, int y, CallbackInfo ci) {
-        this.renderOrderedTooltip(matrices, Lists.transform(doFix(lines, x), Text::asOrderedText), x, y);
+        this.renderOrderedTooltip(matrices, Lists.transform(Helper.doFix(lines, x, width, textRenderer), Text::asOrderedText), x, y);
         ci.cancel();
-    }
-
-    public List<Text> doFix(List<Text> text, int x) {
-        text = new ArrayList<>(text);
-        //text.add(new LiteralText("MOM MY DICK HURTS REALLY BAD").formatted(Formatting.DARK_GREEN, Formatting.UNDERLINE));
-       //text.add(new LiteralText("BEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"));
-        for (int i = 0; i < text.size(); i++) {
-            if (isTooWide(x, width, text.get(i).getString())) {
-                Style style = text.get(i).getStyle();
-                List<String> words = new ArrayList<>(Arrays.asList(text.get(i).getString().split(" ")));
-
-                String newLine = words.remove(0);
-                if (isTooWide(x, width, newLine)) {
-                    String oldLine = newLine;
-                    while (isTooWide(x, width, newLine + "-"))
-                        newLine = newLine.substring(0, newLine.length() - 1);
-                    words.add(0, "-" + oldLine.substring(newLine.length()));
-                    newLine = newLine + "-";
-
-                } else
-                    while (words.size() > 0)
-                        if (!isTooWide(x, width, newLine + " " + words.get(0)))
-                            newLine += " " + words.remove(0);
-                        else break;
-
-                text.set(i, new LiteralText(newLine).setStyle(style));
-                if (words.size() > 0)
-                    text.add(i + 1, new LiteralText(String.join(" ", words)).setStyle(style));
-            }
-        }
-        return text;
-    }
-
-    public boolean isTooWide(int x, int width, String line) {
-        return x + textRenderer.getWidth(line) > width - 20;
     }
 }
