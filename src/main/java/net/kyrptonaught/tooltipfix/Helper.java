4945a7e99611c179fa4061ca7fdea4d19bdbd463
kyrptonaught <anthonybuttillo10@gmail.com>
2020-10-11 04:47:24 -0400

    Now attempts to flip the tooltip to the left side of the mouse to fit

--- src/main/java/net/kyrptonaught/tooltipfix/Helper.java
+++ src/main/java/net/kyrptonaught/tooltipfix/Helper.java
@@ -4,52 +4,76 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Helper {
-    public static List<Text> doFix(List<Text> text, int x, int width, TextRenderer textRenderer) {
+    public static int x, width;
+    private static int mouseX;
+    static boolean flipped;
+
+    public static void set(int x, int width) {
+        Helper.x = Math.max(0, x);
+        Helper.width = width - 20;
+        mouseX = Helper.x - 24;
+        flipped = false; //width - x <= 100; // auto flip if the biggest possible length is less than 100; is this needed?
+    }
+
+    public static List<Text> doFix(List<Text> text, TextRenderer textRenderer) {
+        List<Text> originalText = text;
         text = new ArrayList<>(text);
+
         //System.out.println(text.size() + " " + text.get(0).getString().length() +  " " + x + " " + width + " " + isTooWide(x, width, textRenderer, text.get(0).getString()));
-        if(text.size() == 1 && text.get(0).getString().length() >=12)
+        if (text.size() == 0 || (text.size() == 1 && text.get(0).getString().length() <= 12))
             return text;
         for (int i = 0; i < text.size(); i++) {
-            if (isTooWide(x, width, textRenderer, text.get(i).getString())) {
+            if (isTooWide(textRenderer, text.get(i).getString())) {
                 Style style = text.get(i).getStyle();
                 List<String> words = new ArrayList<>(Arrays.asList(text.get(i).getString().split(" ")));
-
                 if (words.isEmpty()) return text;
 
                 String newLine = words.remove(0);
-                if (isTooWide(x, width, textRenderer, newLine)) {
-                    String oldLine = newLine;
-
-                    while (isTooWide(x, width, textRenderer, newLine + "-")) {
-                        newLine = newLine.substring(0, newLine.length() - 1);
+                if (isTooWide(textRenderer, newLine)) {
+                    if (!flipped && x > width / 2) {
+                        flipped = true;
+                        return doFix(originalText, textRenderer);
+                    } else {
+                        String oldLine = newLine;
+                        while (isTooWide(textRenderer, newLine + "-")) {
+                            newLine = newLine.substring(0, newLine.length() - 1);
+                        }
+                        words.add(0, "-" + oldLine.substring(newLine.length()));
+                        newLine = newLine + "-";
                     }
-                    words.add(0, "-" + oldLine.substring(newLine.length()));
-                    newLine = newLine + "-";
-
                 } else
                     while (words.size() > 0)
-                        if (!isTooWide(x, width, textRenderer, newLine + " " + words.get(0)))
+                        if (!isTooWide(textRenderer, newLine + " " + words.get(0)))
                             newLine += " " + words.remove(0);
                         else break;
-
                 text.set(i, new LiteralText(newLine).setStyle(style));
                 if (words.size() > 0)
                     text.add(i + 1, new LiteralText(String.join(" ", words)).setStyle(style));
             }
         }
-
-
         return text;
     }
 
-    private static boolean isTooWide(int x, int width, TextRenderer textRenderer, String line) {
-        return x + textRenderer.getWidth(line) > width - 20;
+    private static boolean isTooWide(TextRenderer textRenderer, String line) {
+        if (flipped) {
+            if (mouseX - textRenderer.getWidth(line) > 0) {
+                attemptUpdateMaxWidth(line, textRenderer);
+                return false;
+            }
+            return true;
+        }
+        return x + textRenderer.getWidth(line) > width;
+    }
+
+    private static void attemptUpdateMaxWidth(String newLine, TextRenderer textRenderer) {
+        int lineWidth = textRenderer.getWidth(newLine);
+        if (lineWidth > mouseX - x) // new line longer than previous "max width"
+            x = mouseX - lineWidth;
     }
 }
--- src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
+++ src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
@@ -2,11 +2,14 @@ package net.kyrptonaught.tooltipfix.mixin;
 
 import com.google.common.collect.Lists;
 import net.kyrptonaught.tooltipfix.Helper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Mouse;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,13 +30,15 @@ public abstract class FixToolTipMixin {
 
     @Inject(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;II)V", at = @At(value = "HEAD"), cancellable = true)
     public void fix(MatrixStack matrices, Text text, int x, int y, CallbackInfo ci) {
-        this.renderOrderedTooltip(matrices, Lists.transform(Helper.doFix(Collections.singletonList(text), x, width, textRenderer), Text::asOrderedText), x, y);
+        Helper.set(x,width);
+        this.renderOrderedTooltip(matrices, Lists.transform(Helper.doFix(Collections.singletonList(text), textRenderer), Text::asOrderedText), Helper.x, y);
         ci.cancel();
     }
 
     @Inject(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;II)V", at = @At("HEAD"), cancellable = true)
     public void fix(MatrixStack matrices, List<Text> lines, int x, int y, CallbackInfo ci) {
-        this.renderOrderedTooltip(matrices, Lists.transform(Helper.doFix(lines, x, width, textRenderer), Text::asOrderedText), x, y);
+        Helper.set(x,width);
+        this.renderOrderedTooltip(matrices, Lists.transform(Helper.doFix(lines, textRenderer), Text::asOrderedText), Helper.x, y);
         ci.cancel();
     }
 }
