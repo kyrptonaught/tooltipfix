5896cfddb25d0a4f59b6c35c7f7854677d3b1582
kyrptonaught <anthonybuttillo10@gmail.com>
2021-09-24 00:59:56 -0400

    Update PR

--- src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
+++ src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
@@ -13,7 +13,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Collections;
 import java.util.List;
