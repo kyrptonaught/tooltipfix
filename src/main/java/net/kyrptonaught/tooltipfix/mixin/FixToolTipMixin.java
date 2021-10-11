526a1c50942944d56d0ce0bf8dd053737ac90147
kyrptonaught <anthonybuttillo10@gmail.com>
2021-10-11 01:59:19 -0400

    Bump version

--- src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
+++ src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
@@ -11,9 +11,7 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Collections;
 import java.util.List;
