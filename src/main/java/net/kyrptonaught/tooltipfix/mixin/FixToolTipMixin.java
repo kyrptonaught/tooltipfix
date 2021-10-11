2dedc64dea96ade4026cbce1340e4e016b24cb62
kyrptonaught <anthonybuttillo10@gmail.com>
2021-10-11 01:58:51 -0400

    Fix compat with various tool tip adding mods

--- src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
+++ src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
@@ -12,11 +12,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Mixin(Screen.class)
 public abstract class FixToolTipMixin {
@@ -28,25 +30,21 @@ public abstract class FixToolTipMixin {
     @Shadow
     public abstract void renderOrderedTooltip(MatrixStack matrices, List<? extends OrderedText> lines, int x, int y);
 
-    @Inject(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;II)V", at = @At(value = "HEAD"), cancellable = true)
-    public void fix(MatrixStack matrices, Text text, int x, int y, CallbackInfo ci) {
-        Helper.set(x,width);
+    @Redirect(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;II)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;renderOrderedTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;II)V"))
+    public void fixText(Screen screen, MatrixStack matrices, List<? extends OrderedText> stupidLines, int x, int y, MatrixStack matrices2, Text text) {
+        Helper.set(x, width);
         this.renderOrderedTooltip(matrices, Lists.transform(Helper.doFix(Collections.singletonList(text), textRenderer), Text::asOrderedText), Helper.x, y);
-        ci.cancel();
     }
 
-    @Inject(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;II)V", at = @At("HEAD"), cancellable = true)
-    public void fix(MatrixStack matrices, List<Text> lines, int x, int y, CallbackInfo ci) {
-        Helper.set(x,width);
+    @Redirect(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;II)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;renderOrderedTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;II)V"))
+    public void fixTexts(Screen screen, MatrixStack matrices, List<? extends OrderedText> stupidLines, int x, int y, MatrixStack matrices2, List<Text> lines) {
+        Helper.set(x, width);
         this.renderOrderedTooltip(matrices, Lists.transform(Helper.doFix(lines, textRenderer), Text::asOrderedText), Helper.x, y);
-        ci.cancel();
     }
 
-    @Inject(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;Ljava/util/Optional;II)V", at = @At("HEAD"), cancellable = true)
-    public void fix(MatrixStack matrices, List<Text> lines, Optional<TooltipData> data, int x, int y, CallbackInfo ci) {
-        Helper.set(x,width);
-        this.renderOrderedTooltip(matrices, Lists.transform(Helper.doFix(lines, textRenderer), Text::asOrderedText), Helper.x, y);
-        ci.cancel();
+    @Redirect(method = "renderTooltip(Lnet/minecraft/client/util/math/MatrixStack;Ljava/util/List;Ljava/util/Optional;II)V", at = @At(value = "INVOKE", target = "Ljava/util/List;stream()Ljava/util/stream/Stream;"))
+    public Stream<Text> fixWTTData(List<Text> list, MatrixStack matrices, List<Text> lines, Optional<TooltipData> data, int x) {
+        Helper.set(x, width);
+        return Helper.doFix(list, textRenderer).stream();
     }
-
 }
