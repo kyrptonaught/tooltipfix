cbfef4b445eec5505e796f0e7e749de9b85255c1
kyrptonaught <anthonybuttillo10@gmail.com>
2023-06-09 21:54:14 -0400

    Port to 1.20

--- src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
+++ src/main/java/net/kyrptonaught/tooltipfix/mixin/FixToolTipMixin.java
@@ -2,6 +2,7 @@ package net.kyrptonaught.tooltipfix.mixin;
 
 import net.kyrptonaught.tooltipfix.Helper;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.client.gui.tooltip.TooltipPositioner;
@@ -16,25 +17,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.ArrayList;
 import java.util.List;
 
-@Mixin(Screen.class)
+@Mixin(DrawContext.class)
 public abstract class FixToolTipMixin {
-    @Shadow
-    protected TextRenderer textRenderer;
-    @Shadow
-    public int width;
 
-    @ModifyVariable(method = "renderTooltipFromComponents", at = @At(value = "HEAD"), index = 2, argsOnly = true)
+    @Shadow
+    public abstract int getScaledWindowWidth();
+
+    @ModifyVariable(method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;)V", at = @At(value = "HEAD"), index = 2, argsOnly = true)
     public List<TooltipComponent> makeListMutable(List<TooltipComponent> value) {
         return new ArrayList<>(value);
     }
 
-    @Inject(method = "renderTooltipFromComponents", at = @At(value = "INVOKE", target = "Ljava/util/List;size()I", ordinal = 0))
-    public void fix(MatrixStack matrices, List<TooltipComponent> components, int x, int y, TooltipPositioner positioner, CallbackInfo ci) {
-        Helper.newFix(components, textRenderer, x, width);
+    @Inject(method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;)V", at = @At(value = "HEAD"))
+    public void fix(TextRenderer textRenderer, List<TooltipComponent> components, int x, int y, TooltipPositioner positioner, CallbackInfo ci) {
+        Helper.newFix(components, textRenderer, x, getScaledWindowWidth());
     }
 
-    @ModifyVariable(method = "renderTooltipFromComponents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V"), index = 7)
-    public int modifyRenderX(int value, MatrixStack matrices, List<TooltipComponent> components, int x, int y) {
+    @ModifyVariable(method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V"), index = 11)
+    public int modifyRenderX(int value, TextRenderer textRenderer, List<TooltipComponent> components, int x) {
         return Helper.shouldFlip(components, textRenderer, x);
     }
 }
